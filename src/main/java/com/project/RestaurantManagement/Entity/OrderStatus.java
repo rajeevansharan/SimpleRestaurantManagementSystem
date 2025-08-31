0e6cd8487b802949e2a94e55333855c722e915fc
rajeevansharan <rajeevansharan@gmail.com>
2025-08-31 23:08:23 +0530

    Add OrderStatus & UserRole enums and validation dependency

---
+++ src/main/java/com/project/RestaurantManagement/Entity/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.project.RestaurantManagement.Entity;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PREPARING,
+    READY,
+    SERVED,
+    CANCELLED
+}
---
+++ src/main/java/com/project/RestaurantManagement/Entity/UserRole.java
@@ -0,0 +1,7 @@
+package com.project.RestaurantManagement.Entity;
+
+public enum UserRole {
+    ADMIN,
+    MANAGER,
+    STAFF
+}
