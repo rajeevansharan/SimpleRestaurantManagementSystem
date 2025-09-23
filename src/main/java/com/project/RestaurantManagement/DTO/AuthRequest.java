8cc8e3ac16b0ae8c6403a006b06dd8a9ba45ae8c
rajeevansharan <rajeevansharan@gmail.com>
2025-09-23 23:03:58 +0530

    Implement authentication controller with login endpoint
    Added `AuthController` with a `/login` endpoint to handle user authentication. Introduced `AuthRequest` and `AuthResponse` DTOs to manage request and response data. Integrated JWT utility and authentication manager for token generation and credential verification.

---
+++ src/main/java/com/project/RestaurantManagement/DTO/AuthRequest.java
@@ -0,0 +1,16 @@
+package com.project.RestaurantManagement.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/project/RestaurantManagement/DTO/AuthResponse.java
@@ -0,0 +1,8 @@
+package com.project.RestaurantManagement.DTO;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
